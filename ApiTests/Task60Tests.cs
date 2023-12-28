e9e207d561664e7898944bbcd3242248662cccfb
Yury Yaichuk <yury.yaichuk@cellares.com>
2023-12-28 18:04:24 +0100

    added new tests

---
+++ ApiTests/Task60Tests.cs
@@ -0,0 +1,94 @@
+﻿using RestApiAutomationTraining.Helpers;
+using System.Net;
+
+namespace RestApiAutomationTraining.ApiTests;
+
+public class Task60Tests : BaseApiTest
+{
+    [SetUp]
+    protected void TestSetup()
+    {
+        ClearUsers();
+    }
+
+    //Scenario #1         
+    //Given I am authorized user
+    //When I send DELETE request to /users endpoint
+    //And Request body contains user to delete
+    //Then I get 204 response code
+    //And User is deleted
+    //And Zip code is returned in list of available zip codes
+    public void DeleteUser_UserDeleted_Valid()
+    {
+        #region Test pre-setup
+
+        CreateUsers(1, true);
+        var userToDelete = GetUserModels().First();
+
+        #endregion
+
+        var deleteUserResponse = WriteApiActions.DeleteUser(userToDelete);
+
+        Assert.Multiple(() =>
+        {
+            Asserts.AssertStatusCode(deleteUserResponse, HttpStatusCode.NoContent);
+            Assert.That(GetUserModels().Any(u => u.Name == userToDelete.Name), Is.False);
+            Assert.That(GetZipCodesModel().Single(zip => zip == userToDelete.ZipCode), Is.Not.Null);
+        });
+    }
+
+    /*
+     * Scenario #2
+     * Given I am authorized user
+     * When I send DELETE request to /users endpoint
+     * And Request body contains user to delete(required fields only)
+     * Then I get 204 response code
+     * And User is deleted
+     * And Zip code is returned in list of available zip codes
+    */
+    public void DeleteUser_UserDeleted_Valid()
+    {
+        #region Test pre-setup
+
+        CreateUsers(1, true);
+        var userToDelete = GetUserModels().First();
+
+        #endregion
+
+        var deleteUserResponse = WriteApiActions.DeleteUser(userToDelete);
+
+        Assert.Multiple(() =>
+        {
+            Asserts.AssertStatusCode(deleteUserResponse, HttpStatusCode.NoContent);
+            Assert.That(GetUserModels().Any(u => u.Name == userToDelete.Name), Is.False);
+            Assert.That(GetZipCodesModel().Single(zip => zip == userToDelete.ZipCode), Is.Not.Null);
+        });
+    }
+
+    /*
+     * Scenario #3
+     * Given I am authorized user
+     * When I send DELETE request to /users endpoint
+     * And Request body contains user to delete(any required field is missed)
+     * Then I get 409 response code
+     * And User is not deleted
+     */
+    public void DeleteUser_UserDeleted_Valid()
+    {
+        #region Test pre-setup
+
+        CreateUsers(1, true);
+        var userToDelete = GetUserModels().First();
+
+        #endregion
+
+        var deleteUserResponse = WriteApiActions.DeleteUser(userToDelete);
+
+        Assert.Multiple(() =>
+        {
+            Asserts.AssertStatusCode(deleteUserResponse, HttpStatusCode.NoContent);
+            Assert.That(GetUserModels().Any(u => u.Name == userToDelete.Name), Is.False);
+            Assert.That(GetZipCodesModel().Single(zip => zip == userToDelete.ZipCode), Is.Not.Null);
+        });
+    }
+}
--- Helpers/Asserts.cs
+++ Helpers/Asserts.cs
@@ -1,4 +1,6 @@
 ﻿using RestSharp;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RestApiAutomationTraining.Helpers;
 
@@ -7,6 +9,13 @@ public class Asserts
     public static void AssertStatusCode(RestResponse response, int expectedCode)
     {
         var actualStatusCode = (int)response.StatusCode;
+        Assert.That(actualStatusCode, Is.EqualTo(expectedCode),
+            $"Wrong StatusCode for endpoint: [{response.Request.Method} {response.ResponseUri}]");
+    }
+
+    public static void AssertStatusCode(RestResponse response, HttpStatusCode expectedCode)
+    {
+        var actualStatusCode = response.StatusCode;
         Assert.That(actualStatusCode, Is.EqualTo(expectedCode),
             $"Wrong StatusCode for endpoint: [{response.Request.Method} {response.ResponseUri}]");
     }
